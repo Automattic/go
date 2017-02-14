fcba99de279c3a53f4fcf5045fbd7f93ce8ee19e
apokalyptik <apokalyptik@apokalyptik.com>
2017-02-14 18:09:58 +0000

    use gocheck to make tests signifigantly less verbose...

--- php/unserialize_test.go
+++ php/unserialize_test.go
@@ -1,275 +1,147 @@
 package php
 
 import (
-	"bytes"
-	"log"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestReferences(t *testing.T) {
-	val, err := Unmarshal([]byte(`a:12:{i:0;i:1;i:1;i:2;i:2;O:8:"stdClass":2:{s:2:"id";s:9:"testClass";s:4:"some";s:5:"thing";}i:3;i:4;i:4;i:5;i:5;a:2:{i:0;s:3:"foo";i:1;s:3:"bar";}i:6;i:7;i:7;i:8;i:8;r:4;i:9;i:10;i:10;i:11;i:11;R:9;}`))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !val.IsArray() {
-		t.Error("not an array?")
-		return
-	}
+func Test(t *testing.T) { TestingT(t) }
 
-	// Object references
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func (t *TestSuite) TestReferences(c *C) {
+	val, err := Unmarshal([]byte(`a:12:{i:0;i:1;i:1;i:2;i:2;O:8:"stdClass":2:{s:2:"id";s:9:"testClass";s:4:"some";s:5:"thing";}i:3;i:4;i:4;i:5;i:5;a:2:{i:0;s:3:"foo";i:1;s:3:"bar";}i:6;i:7;i:7;i:8;i:8;r:4;i:9;i:10;i:10;i:11;i:11;R:9;}`))
+
+	c.Assert(err, IsNil)
+	c.Assert(val.IsArray(), Equals, true)
 	row8, err := val.GetKey(8)
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if row8.kind != KindObjReference {
-		t.Error("should be object reference")
-		return
-	}
+	c.Assert(err, IsNil)
+	c.Assert(row8.kind, Equals, KindObjReference)
 	row8resolve, err := row8.GetKey("some")
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if row8resolve.kind != KindString {
-		t.Error("should resolve to a string!")
-		return
-	}
-	if s, _ := row8resolve.String(); s != "thing" {
-		t.Error("should have resolved to 'thing'")
-	}
+	c.Assert(err, IsNil)
+	c.Assert(row8resolve.kind, Equals, KindString)
+	s, err := row8resolve.String()
+	c.Assert(err, IsNil)
+	c.Assert(s, Equals, "thing")
 
 	// Variable references
 	row11, err := val.GetKey(11)
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if row11.kind != KindVarReference {
-		t.Error("should be variable reference")
-		return
-	}
+	c.Assert(err, IsNil)
+	c.Assert(row11.kind, Equals, KindVarReference)
 	row11resolve, err := row11.GetKey(1)
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if row11resolve.kind != KindString {
-		t.Error("should resolve to a string!")
-		return
-	}
-	if s, _ := row11resolve.String(); s != "bar" {
-		t.Error("should have resolved to 'bar'")
-	}
-
+	c.Assert(err, IsNil)
+	c.Assert(row11resolve.kind, Equals, KindString)
+	s, err = row11resolve.String()
+	c.Assert(err, IsNil)
+	c.Assert(s, Equals, "bar")
 }
 
-func TestObject(t *testing.T) {
+func (t *TestSuite) TestObject(c *C) {
 	val, err := Unmarshal(
 		[]byte("O:3:\"Foo\":4:{s:3:\"one\";s:3:\"aaa\";s:6:\"\000*\000two\";s:3:\"bbb\";s:10:\"\000Foothree\000\";s:3:\"ccc\";s:4:\"four\";s:3:\"ddd\";}"),
 	)
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
+	c.Assert(err, IsNil)
 	j, _ := val.JSON()
 	// need better things here. this will fail at some point. maybe just on a different machine. due to map randomization....
-	if !bytes.Equal(j, []byte(`{"four":"ddd","one":"aaa","three":"ccc","two":"bbb"}`)) {
-		t.Error("did not marshal to expected json...")
-	}
+	c.Assert(string(j), Equals, `{"four":"ddd","one":"aaa","three":"ccc","two":"bbb"}`)
 }
 
-func TestNullBoolOffsets(t *testing.T) {
+func (t *TestSuite) TestNullBoolOffsets(c *C) {
 	a, err := Unmarshal([]byte("a:7:{i:0;b:1;i:1;b:0;i:2;b:0;i:3;b:1;i:4;N;i:5;N;i:6;s:4:\"addd\";}"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !a.IsArray() {
-		t.Error("something went wrong")
-	}
-	if array, _ := a.Rows(); len(array) != 7 {
-		log.Printf("%#v", array)
-		t.Error("something else went wrong")
-	}
+	c.Assert(err, IsNil)
+	c.Assert(a.IsArray(), Equals, true)
+	array, _ := a.Rows()
+	c.Assert(len(array), Equals, 7)
 }
 
-func TestNull(t *testing.T) {
+func (t *TestSuite) TestNull(c *C) {
 	n, err := Unmarshal([]byte("N;"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !n.IsNull() {
-		t.Error("is not null")
-		return
-	}
+	c.Assert(err, IsNil)
+	c.Assert(n.IsNull(), Equals, true)
 }
 
-func TestBool(t *testing.T) {
+func (t *TestSuite) TestBool(c *C) {
 	bt, err := Unmarshal([]byte("b:1;"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !bt.IsBool() {
-		t.Error("is not boolean")
-		return
-	}
-	if b, _ := bt.Bool(); !b {
-		t.Error("shoulf be true")
-	}
-
+	c.Assert(err, IsNil)
+	c.Assert(bt.IsBool(), Equals, true)
+	b, _ := bt.Bool()
+	c.Assert(b, Equals, true)
 	f, err := Unmarshal([]byte("b:0;"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !f.IsBool() {
-		t.Error("is not boolean")
-		return
-	}
-	if b, _ := f.Bool(); b {
-		t.Error("shoulf be false")
-	}
+	c.Assert(err, IsNil)
+	c.Assert(f.IsBool(), Equals, true)
+	b, _ = f.Bool()
+	c.Assert(b, Equals, false)
 }
 
-func TestNestedArray(t *testing.T) {
+func (t *TestSuite) TestNestedArray(c *C) {
 	val, err := Unmarshal([]byte("a:3:{i:0;i:1;i:1;a:2:{i:0;i:2;i:1;i:3;}i:2;i:4;}"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !val.IsArray() {
-		t.Error("not an array")
-		return
-	}
+	c.Assert(err, IsNil)
+	c.Assert(val.IsArray(), Equals, true)
 	shouldBeArray, err := val.GetKey(1)
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-
-	if !shouldBeArray.IsArray() {
-		t.Error("sub-array not an array")
-		return
-	}
-
+	c.Assert(err, IsNil)
+	c.Assert(shouldBeArray.IsArray(), Equals, true)
 	v, _ := shouldBeArray.GetKey(1)
-	if i, _ := v.Int(); i != 3 {
-		t.Error("wrong value")
-		return
-	}
+	i, _ := v.Int()
+	c.Assert(i, Equals, 3)
 }
 
-func TestSimpleArray(t *testing.T) {
+func (t *TestSuite) TestSimpleArray(c *C) {
 	val, err := Unmarshal([]byte("a:3:{i:0;i:1;i:1;i:2;i:2;i:3;}"))
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	if !val.IsArray() {
-		t.Error("not an array")
-		return
-	}
+	c.Assert(err, IsNil)
 	array, err := val.Rows()
-	if err != nil {
-		t.Error(err.Error())
-		return
-	}
-	array[0].Key.Int()
-	if i, err := array[1].Key.Int(); err != nil {
-		t.Error(err.Error())
-		return
-	} else if i != 1 {
-		t.Error("wrong index value")
-		return
-	}
-	if i, err := array[2].Val.Int(); err != nil {
-		t.Error(err.Error())
-		return
-	} else if i != 3 {
-		t.Error("wrong value")
-		return
-	}
+	c.Assert(err, IsNil)
+	i, err := array[1].Key.Int()
+	c.Assert(err, IsNil)
+	c.Assert(i, Equals, 1)
+	i, err = array[2].Val.Int()
+	c.Assert(err, IsNil)
+	c.Assert(i, Equals, 3)
 }
 
-func TestFloat(t *testing.T) {
+func (t *TestSuite) TestFloat(c *C) {
 	tests := map[float64][]byte{
 		10.99:     []byte("d:10.99;"),
 		999999.99: []byte("d:999999.99;"),
 	}
 	for want, input := range tests {
 		v, err := Unmarshal(input)
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if !v.IsFloat() {
-			t.Error("is not a float")
-			return
-		}
+		c.Assert(err, IsNil)
+		c.Assert(v.IsFloat(), Equals, true)
 		s, err := v.Float()
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if s != want {
-			t.Error("Content decoded to the wrong string")
-		}
+		c.Assert(err, IsNil)
+		c.Assert(s, Equals, want)
 	}
 }
 
-func TestInt(t *testing.T) {
+func (t *TestSuite) TestInt(c *C) {
 	tests := map[int][]byte{
 		1:      []byte("i:1;"),
 		100009: []byte("i:100009;"),
 	}
 	for want, input := range tests {
 		v, err := Unmarshal(input)
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if !v.IsInt() {
-			t.Error("not an integer")
-			return
-		}
+		c.Assert(err, IsNil)
+		c.Assert(v.IsInt(), Equals, true)
 		s, err := v.Int()
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if s != want {
-			t.Error("Content decoded to the wrong string")
-		}
+		c.Assert(err, IsNil)
+		c.Assert(s, Equals, want)
 	}
 }
 
-func TestString(t *testing.T) {
+func (t *TestSuite) TestString(c *C) {
 	tests := map[string][]byte{
 		"foobarbazboo":            []byte(`s:12:"foobarbazboo";`),
 		"fo\to\nb\ra\"r'b;aüzboo": []byte("s:20:\"fo\to\nb\ra\"r'b;aüzboo\";"),
 	}
 	for want, input := range tests {
-		t.Logf("Testing for %#v", want)
 		v, err := Unmarshal(input)
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if !v.IsString() {
-			t.Error("is not a string")
-			return
-		}
+		c.Assert(err, IsNil)
+		c.Assert(v.IsString(), Equals, true)
 		s, err := v.String()
-		if err != nil {
-			t.Error(err.Error())
-			return
-		}
-		if s != want {
-			t.Error("Content decoded to the wrong string")
-		}
+		c.Assert(err, IsNil)
+		c.Assert(s, Equals, want)
 	}
 }
